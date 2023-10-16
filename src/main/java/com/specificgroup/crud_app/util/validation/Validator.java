1011603adc058bd81c189d09324b0e120f809cc5
VarvaraMamatsiuk <varvara.mamatsiuk@specific-group.com>
2023-10-16 12:36:44 +0300

    Refactoring: Deleted unnecessary class

--- src/main/java/com/specificgroup/crud_app/util/validation/Validator.java
+++ src/main/java/com/specificgroup/crud_app/util/validation/Validator.java
@@ -38,15 +38,4 @@ public class Validator<T> {
     public List<String> getMessages(){
         return messages;
     }
-
-    class RegularExpression {
-        public static final String NAME_VALIDATION = "[a-zA-Z]+";
-        public static final String AGE_VALIDATION = "\\d{1,2}";
-        public static final String EMAIL_VALIDATION = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
-        public static final String PHONE_VALIDATION = "\\+\\d{12}";
-
-
-
-        public static final long INVALID_RESULT = -1;
-    }
 }
